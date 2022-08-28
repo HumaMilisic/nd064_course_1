c85cb5ed1d59f37dfa1dd9c45f9efec034fe0665
HumaMilisic <huma.milisic@gmail.com>
2022-08-28 20:57:03 +0200

    chore: old off

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,38 @@
 from flask import Flask
+from flask import json
+
+import logging
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+
+    app.logger.info('its hello')
     return "Hello World!"
 
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('its status')
+    return response
+    # return "status"
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 23}}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('its metrics')
+    return response
+    # return "metrics"
+
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
